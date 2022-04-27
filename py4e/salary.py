c725119ff9a03b969cf0ab8d092d20837ef7a8af
Anrich Joubert <100349292+Anrich05@users.noreply.github.com>
2022-04-27 16:17:54 +0200

    Add files via upload

---
+++ py4e/salary.py
@@ -0,0 +1,7 @@
+hours = input("Enter hours worked: ")
+hours = int(hours)
+rate = input("Enter the rate of pay: ")
+rate = float(rate)
+salary = 0
+salary = hours * rate
+print("Your salary is: $" , salary)
